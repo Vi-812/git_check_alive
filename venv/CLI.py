fde253addaa667e0119a1806ca1d169ca745ef87
Vi <vi812x@gmail.com>
2022-12-01 14:23:58 +0300

    Except 3

--- venv/CLI.py
+++ venv/CLI.py
@@ -4,31 +4,42 @@ import re
 
 
 while True:
-    adr = input('Вставьте ссылку на репозиторий (q - выход): ')
 
-    if adr == 'q' or adr == 'Q':
-        sys.exit()
 
-    if adr == ' ':
-        adr = 'https://github.com/Vi-812/GIT'
+    while True:
+        adr = input('Вставьте ссылку на репозиторий (q - выход): ')
 
-    adress = re.search('github.com/(.+/.+)', adr)
-    if adress:
-        adress = adress.group(1)
-    else:
-        adress = re.search('//?(.+/.+)', adr)
+        if adr == 'q' or adr == 'Q':
+            sys.exit()
+
+        if adr == ' ':
+            adr = 'https://github.com/Vi-812/GIT'
+
+        adress = re.search('github.com/(.+/.+)', adr)
         if adress:
             adress = adress.group(1)
+        else:
+            adress = re.search('(.+/.+)', adr)
+            if adress:
+                adress = adress.group(1)
 
-    if adress:
-        break
-    else:
-        print('Ссылка не корректна, введите ссылку в формате')
-        print('"https://github.com/Vi-812/GIT" либо "Vi-812/GIT"')
-        print('Либо введите Q для выхода')
+        if adress:
+            break
+        else:
+            print('Ссылка не корректна, введите ссылку в формате')
+            print('"https://github.com/Vi-812/GIT" либо "Vi-812/GIT"')
+            print('Либо введите q для выхода')
+            print('')
 
-g = Github()
-repo = g.get_repo(adress)
-print(f'Наименование: {repo.name}')
-print(f'Описание: {repo.description}')
-print(f'Рейтинг: {repo.stargazers_count}')
+    try:
+        g = Github()
+        repo = g.get_repo(adress)
+        print(f'Наименование: {repo.name}')
+        print(f'Описание: {repo.description}')
+        print(f'Рейтинг: {repo.stargazers_count}')
+        print('')
+
+    except (GithubException, UnknownObjectException, NameError):
+        print('Указанного вами репозитория не существует')
+        print('Повторите попытку либо введите q для выхода')
+        print('')
