eef158f77a67176304c620f786d0c8e06b2b8c23
Vi <vi812x@gmail.com>
2022-12-22 13:31:19 +0300

    Улучшена обработка исключений 5.2

--- cli_git_api.py
+++ cli_git_api.py
@@ -9,36 +9,36 @@ def createParser():
     parser.add_argument('repository', nargs='?')
     return parser
 
-if __name__ == '__main__':
-    parser = createParser()
-    namespace = parser.parse_args()
 
-    if not namespace.repository:
-        print('Укажите ссылку либо имя репозитория')
-        sys.exit()
-    if namespace.repository == '1':
-        namespace.repository = 'https://github.com/Vi-812/git_check_alive'
-    # print(namespace.repository)
+parser = createParser()
+namespace = parser.parse_args()
 
-    adress = re.search('([^/]+/[^/]+)$', namespace.repository)
-    if adress:
-        adress = adress.group(1)
-    # print(adress)
-    if not adress:
-        print('Ссылка не корректна, введите ссылку в формате')
-        print('"https://github.com/Vi-812/GIT" либо "Vi-812/GIT"')
-        sys.exit()
-    # print(adress)
+if not namespace.repository:
+    print('Укажите ссылку либо имя репозитория')
+    sys.exit()
+if namespace.repository == '1':
+    namespace.repository = 'https://github.com/Vi-812/git_check_alive'
 
-    try:
-        g = Github()
-        repo = g.get_repo(adress)
-        print(f'Наименование: {repo.name}')
-        print(f'Описание: {repo.description}')
-        print(f'Рейтинг: {repo.stargazers_count}')
+adress = re.search('([^/]+/[^/]+)$', namespace.repository)
+if adress:
+    adress = adress.group(1)
+if not adress:
+    print('Ссылка не корректна, введите ссылку в формате')
+    print('"https://github.com/Vi-812/GIT" либо "Vi-812/GIT"')
+    sys.exit()
 
-    except UnknownObjectException:
-        print('Указанного вами репозитория не существует')
+try:
+    g = Github()
+    repo = g.get_repo(adress)
+    print(f'Наименование: {repo.name}')
+    print(f'Описание: {repo.description}')
+    print(f'Рейтинг: {repo.stargazers_count}')
 
-    except:
-        print('Ошибка, возможно нет интернета =)')
+except UnknownObjectException:
+    print('Указанного вами репозитория не существует')
+
+except Exception as error:
+    print('Ошибка:')
+    print(error.__class__)
+    print(error.__context__)
+
