300f9bdab592f78cdddc5b876a6fc62036f74e6f
Vi <vi812x@gmail.com>
2023-01-13 22:32:20 +0300

    v 0.7.02 s

--- analyzer.py
+++ analyzer.py
@@ -15,53 +15,85 @@ class GitGraphql:
         self.repository_name = 'jest'
 
     def get_info(self):
+        self.cursor = 'null'
+        self.labels_name = []
 
-        data = {
-            "owner": "facebook",
-            "name": "jest"
-            }
-
-        print(data)
-
-        self.json = {
-            'query': 'query GetInfo ($owner: String = "facebook", $name: String = "jest") {'
-                'repository(name: $name, owner: $owner) {'
-                    'name\n'
-                    'description\n'
-                    'stargazerCount\n'
-                    'createdAt\n'
-                    'updatedAt\n'
-                    'isArchived\n'
-                    'labels(first: 2, after: "NA") {' # Встроить переменные
-                        'totalCount\n'
-                        'pageInfo {'
-                            'startCursor\n'
-                            'endCursor\n'
-                            'hasNextPage'
-                        '}'
-                        'edges {'
-                            'cursor\n'
-                            'node {'
-                                'name'
+        while True:
+            self.json = {
+                'query': 'query GetInfo ($owner: String!, $name: String!, $cursor: String = null) {'
+                    'repository(name: $name, owner: $owner) {'
+                        'name '
+                        'description '
+                        'stargazerCount '
+                        'createdAt '
+                        'updatedAt '
+                        'isArchived '
+                        'labels(first: 100, after: $cursor) {'
+                            'totalCount '
+                            'pageInfo {'
+                                'startCursor '
+                                'endCursor '
+                                'hasNextPage'
+                            '}'
+                            'edges {'
+                                'cursor '
+                                'node {'
+                                    'name'
+                                '}'
                             '}'
                         '}'
                     '}'
-                '}'
-                'rateLimit {'
-                    'cost\n'
-                    'remaining'
-                '}'
-            '}'
-            # """
-            # variables {
-            #     "owner": "facebook"
-            # }
-            # """
-        }
+                    'rateLimit {'
+                        'cost '
+                        'remaining '
+                         'resetAt'
+                    '}'
+                '}',
+                'variables': {
+                    "owner": self.repository_owner,
+                    "name": self.repository_name,
+                    "cursor": self.cursor
 
-        data = requests.post(url=self.url, headers=self.headers, json=self.json)
-        data = data.json()
-        print(data)
+                }
+            }
+            # print(self.json)
+
+            data = requests.post(url=self.url, headers=self.headers, json=self.json)
+            data = data.json()
+            # print(data)
+
+            self.name = data['data']['repository']['name']
+            self.description = data['data']['repository']['description']
+            self.stars = data['data']['repository']['stargazerCount']
+            self.created_at = data['data']['repository']['createdAt']
+            self.updated_at = data['data']['repository']['updatedAt']
+            self.archived = data['data']['repository']['isArchived']
+            self.labels_count = data['data']['repository']['labels']['totalCount']
+            self.start_cursor = data['data']['repository']['labels']['pageInfo']['startCursor']
+            self.end_cursor = data['data']['repository']['labels']['pageInfo']['endCursor']
+            self.has_next_page = data['data']['repository']['labels']['pageInfo']['hasNextPage']
+            for label in data['data']['repository']['labels']['edges']:
+                self.labels_name.append(label['node']['name'])
+            self.request_cost = data['data']['rateLimit']['cost']
+            self.request_balance = data['data']['rateLimit']['remaining']
+            self.request_reset = data['data']['rateLimit']['resetAt']
+            if self.has_next_page:
+                self.cursor = self.end_cursor
+            else:
+                break
+
+        print(self.labels_count, '>>', self.labels_name)
+
+        self.labels_bug = []
+        for name in self.labels_name:
+            if 'bug' in name:
+                self.labels_bug.append(name)
+
+        print(self.labels_bug)
+
+
+
+
 
 x = GitGraphql()
 x.get_info()
