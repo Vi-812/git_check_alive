f338babfece47ab49d0c8603e44d8a0e12c080e3
Vi <vi812x@gmail.com>
2023-01-26 23:10:59 +0300

    Правки save

--- cli.py
+++ cli.py
@@ -5,8 +5,6 @@ import github_api_client as ga
 from dotenv import load_dotenv
 load_dotenv()
 
-debug = True
-
 token = os.getenv('TOKEN')
 
 try:
@@ -20,15 +18,10 @@ except:
     print('"https://github.com/Vi-812/git" либо "Vi-812/git"')
     sys.exit()
 
-# DEBUG TRUE
 if not namespace.repository_path:
-    if not debug:
-        print('Передайте аргументом ссылку или владельца/имя репозитория')
-        print('"https://github.com/Vi-812/git" либо "Vi-812/git"')
-        sys.exit()
-    else:
-        namespace.repository_path = 'https://github.com/pallets/flask'
-        # namespace.repository_path = 'https://github.com/Vi-812/git_check_alive'
+    print('Передайте аргументом ссылку или владельца/имя репозитория')
+    print('"https://github.com/Vi-812/git" либо "Vi-812/git"')
+    sys.exit()
 
 instance_api_client = ga.GithubApiClient(token)
 return_json = instance_api_client.get_report(namespace.repository_path)
