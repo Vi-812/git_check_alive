75469aa5eff597768cc1d4a1f8c08eee1256e8c7
Vi <vi812x@gmail.com>
2023-02-15 12:49:05 +0300

    Async 3

--- app/views.py
+++ app/views.py
@@ -21,21 +21,21 @@ def api_request():
 
 @app_flask.route('/', methods=['GET', 'POST'])
 async def main_page():
-    session = aiohttp.ClientSession()
-    print(session.__dict__)
-    if request.method == 'GET':
-        form = forms.RepositoryPathForm()
-        return render_template('index.html', form=form), 200
-    elif request.method == 'POST':
-        form = forms.RepositoryPathForm()
-        repository_path = request.form['link_repository']
-        instance_db_client = database.DataBaseHandler()
-        instance_db_client.get_report(repository_path, token_flask)
-        code = resp_json.query_info.code
-        if code != 200:
-            resp_json.__delattr__('repository_info')
-            resp_json.__delattr__('analytic')
-        return render_template('index.html', form=form, json=resp_json.json(by_alias=True)), code
+    async with aiohttp.ClientSession() as session:
+        if request.method == 'GET':
+            form = forms.RepositoryPathForm()
+            return render_template('index.html', form=form), 200
+        elif request.method == 'POST':
+            form = forms.RepositoryPathForm()
+            repository_path = request.form['link_repository']
+            instance_db_client = database.DataBaseHandler()
+            instance_db_client.get_report(repository_path, token_flask)
+            await asyncio.sleep(0)
+            code = resp_json.query_info.code
+            if code != 200:
+                resp_json.__delattr__('repository_info')
+                resp_json.__delattr__('analytic')
+            return render_template('index.html', form=form, json=resp_json.json(by_alias=True)), code
 
 
 @app_flask.errorhandler(404)
